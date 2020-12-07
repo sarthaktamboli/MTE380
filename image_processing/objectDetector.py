3a8643303dc0ddb94e64258dd8ce724971a9fa65
qinyang <q7bao@edu.uwaterloo.ca>
2020-12-07 11:40:26 -0500

    add more arguments for object detector, and update README

--- image_processing/objectDetector.py
+++ image_processing/objectDetector.py
@@ -3,6 +3,7 @@ import numpy as np
 import argparse
 import cv2
 import time
+import os
 from imutils.video import VideoStream
 from skimage.transform import ProjectiveTransform
 
@@ -20,6 +21,10 @@ ap.add_argument("-c", "--confidence", type=float, default=0,
 	help="minimum probability to filter weak detections")
 ap.add_argument("-s", "--skip-frames", type=int, default=1,
 	help="# of skip frames between detections")
+ap.add_argument("-t", "--measure-times", action='store_true',
+	help="flag for whether to measure processing time or not")
+ap.add_argument("-g", "--draw-grids", action='store_true',
+	help="flag for whether to draw grids, only works for second video currently")
 args = vars(ap.parse_args())
 
 # initialize the list of class labels MobileNet SSD was trained to
@@ -40,6 +45,11 @@ else:
 	vs = cv2.VideoCapture(args["input"])
 # load our serialized model from disk
 
+if args.get("draw_grids", False) and args.get("input", False):
+	if os.path.abspath(args["input"]) != os.path.abspath(os.path.join("input_videos", "2.mp4")):
+		sys.exit("Error, drawing grids only works for input_videos/2.mp4")
+
+
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
@@ -52,6 +62,7 @@ projectiveTransform = ProjectiveTransform()
 projectiveTransform.estimate(src, dst)
 
 times = []
+cellBorders = None
 
 totalFrames = 0
 while True:
@@ -75,7 +86,8 @@ while True:
 		net.setInput(blob)
 		start2 = time.time()
 		detections = net.forward()
-		print("[INFO] Forward Propragation: {:.4f}s".format(time.time() - start2))
+		if args["measure_times"]:
+			print("[INFO] Forward Propragation: {:.4f}s".format(time.time() - start2))
 
 		# loop over the detections
 		for i in np.arange(detections.shape[2]):
@@ -104,35 +116,71 @@ while True:
 				y = startY - 15 if startY - 15 > 15 else startY + 15
 				cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-		times.append(time.time() - start1)
-		print("[INFO] Total detection time: {:.4f}s".format(times[0-1]))
+		if args["measure_times"]:
+			times.append(time.time() - start1)
+			print("[INFO] Total detection time: {:.4f}s".format(times[0-1]))
 
 
 		'''
 		alpha = 0.25
 		overlay = frame.copy()
-
 		cv2.line(overlay, (0, 269), (w, 269), (0, 255, 0), 2)
 		cv2.line(overlay, (53, 0),  (53, h),  (0, 255, 0), 2)
-
 		cv2.line(overlay, (0, 238), (w, 238), (255, 0, 0), 2)
 		cv2.line(overlay, (225, 0), (225, h), (255, 0, 0), 2)
-
 		cv2.line(overlay, (0, 318), (w, 318), (0, 255, 255), 2)
 		cv2.line(overlay, (190, 0), (190, h), (0, 255, 255), 2)
-
 		cv2.line(overlay, (0, 280), (w, 280), (0, 0, 255), 2)
 		cv2.line(overlay, (365, 0), (365, h), (0, 0, 255), 2)
-
 		cv2.addWeighted(overlay, alpha, frame , 1 - alpha, 0, frame)
 		'''
 
-		
-		# cv2.line(frame, tuple(dst[0]), tuple(dst[1]), (0, 255, 0), 2)		# green
-		# cv2.line(frame, tuple(dst[0]), tuple(dst[2]), (255, 0, 0), 2)		# blue
-		# cv2.line(frame, tuple(dst[2]), tuple(dst[3]), (0, 0, 255), 2)		# red
-		# cv2.line(frame, tuple(dst[1]), tuple(dst[3]), (0, 255, 255), 2)		# yellow
-		
+		'''
+		cv2.line(frame, tuple(dst[0]), tuple(dst[1]), (0, 255, 0), 2)		# green
+		cv2.line(frame, tuple(dst[0]), tuple(dst[2]), (255, 0, 0), 2)		# blue
+		cv2.line(frame, tuple(dst[2]), tuple(dst[3]), (0, 0, 255), 2)		# red
+		cv2.line(frame, tuple(dst[1]), tuple(dst[3]), (0, 255, 255), 2)		# yellow
+		'''
+
+		if args["draw_grids"]:
+			if cellBorders is None:
+				cellBorders = []
+
+				grid = [[-1,  0,  1, -1, -1, -1, -1,  0,  0, -1, -1, -1, -1, -1, -1,  1, -1, -1, -1,  1],
+						[-1,  0,  1,  1,  1, -1, -1, -1,  0,  2,  2,  2,  2,  2,  4,  4, -1, -1, -1,  1],
+						[-1,  0,  0,  0,  1,  4,  2,  2,  4,  2,  3,  3,  3,  3,  4,  2,  2,  4,  1,  1],
+						[-1, -1, -1,  0,  0,  4, -1, -1,  4,  3,  3, -1, -1, -1,  3,  3,  4,  4,  2, -1],
+						[-1, -1, -1, -1, -1,  2, -1, -1,  3, -1, -1, -1, -1, -1, -1, -1,  3,  3,  2,  2],
+						[-1, -1, -1, -1, -1,  4,  4,  3,  3, -1, -1, -1, -1, -1, -1, -1, -1,  3,  3,  2],
+						[-1, -1, -1, -1, -1,  2,  3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  3,  2],
+						[-1, -1, -1, -1,  2,  2,  3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  3,  2],
+						[-1, -1, -1, -1,  2,  3,  3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  4,  4],
+						[-1, -1, -1, -1,  2,  3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  3,  2],
+						[-1, -1, -1, -1,  2,  3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  3,  2],
+						[ 1,  1,  1,  1,  4,  4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  3,  2],
+						[-1, -1,  0,  0,  4,  4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  3,  2],
+						[ 0,  0,  0, -1,  2,  3,  3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  3,  2],
+						[-1, -1, -1, -1,  2,  4,  4,  3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  3,  2],
+						[-1, -1, -1, -1,  1,  4,  2,  3,  3, -1, -1, -1, -1, -1, -1, -1, -1,  3,  3,  2],
+						[-1, -1, -1, -1,  1,  0,  2,  2,  4,  3,  3,  3,  4,  3, -1, -1,  3,  3,  2,  2],
+						[-1, -1, -1,  1,  1,  0, -1,  4,  4,  2,  2,  2,  4,  3,  3,  4,  3,  2,  2, -1],
+						[-1,  1,  1,  1,  0,  0, -1,  1, -1, -1, -1, -1,  2,  2,  2,  4,  2,  2, -1, -1],
+						[-1,  1,  0,  0,  0, -1, -1,  1, -1, -1, -1, -1, -1, -1, -1,  0, -1, -1, -1, -1],
+						[-1,  1,  0, -1, -1, -1, -1,  1, -1, -1, -1, -1, -1, -1, -1,  0, -1, -1, -1, -1]]
+
+				floormap = np.array(grid).transpose(1,0)
+
+				for i, j in np.ndindex(h, w):
+					relativeLoc = tuple(projectiveTransform.inverse(np.array([[j, i]]))[0])
+					nearestBorder = (200 * round(relativeLoc[0] / 200), 200 * round(relativeLoc[1] / 200))
+					if abs(relativeLoc[0] - nearestBorder[0]) <= 5 or abs(relativeLoc[1] - nearestBorder[1]) <= 5:
+						gridLoc = (5 + int(relativeLoc[0] // 200), 11 + int(relativeLoc[1] // 200))
+						if floormap[gridLoc] != -1:
+							cellBorders.append((i, j))
+				# print(len(cellBorders))
+
+			for cellBorder in cellBorders:
+				frame[cellBorder] = (0, 255, 255)
 
 		cv2.imshow("Frame", frame)
 
@@ -157,8 +205,9 @@ else:
 if writer is not None:
 	writer.release()
 
-print("[INFO] Average total detection time: {}".format(np.average(times)))
-print("[INFO] Total detection time std. dev: {}".format(np.std(times)))
+if args["measure_times"]:
+	print("[INFO] Average total detection time: {}".format(np.average(times)))
+	print("[INFO] Total detection time std. dev: {}".format(np.std(times)))
 
 #0.026670806407928467, 0.005443000949946635 (1.mp4)
 #0.02047379522134137, 0.003309995200970159 (2.mp4)
